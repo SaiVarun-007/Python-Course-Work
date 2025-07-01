33342ed9f56b05f76108a998b6bec439aeb5b2b5
8530833475 <varun.pillalamarri@msds.christuniversity.in>
2025-07-01 11:46:41 +0530

    Hello

---
+++ Class work/VariablesRulesandAssignments.py
@@ -0,0 +1,15 @@
+myvar=10 #valid Var
+Myvar=10 #valid Var
+#1myvar=10 ** SyntaxError: invalid syntax
+#@myvar=10 ** SyntaxError: invalid syntax
+my_var=10 #valid Var
+#my@var=10 ** [SyntaxError]
+
+myvar=10 #valid Var
+Myvar=10 #valid Var
+myvar1=10 #valid Var
+_myvar=10 #valid Var
+
+myVar=10 #case-sensistive
+Myvar=20 #case-sensitive
+print(myvar,Myvar)#10 20
