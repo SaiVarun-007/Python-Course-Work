59abc1cf142257934770cbfb7bafec3072b54288
8530833475 <varun.pillalamarri@msds.christuniversity.in>
2025-07-02 11:25:06 +0530

    PythonOperators

---
+++ Classwork/5.PythonOperators.py
@@ -0,0 +1,11 @@
+#1.Arithmetic Operators
+
+a=10
+b=20
+print("Addition(+) :", a+b)
+print("Subtraction(-) :", a-b)
+print("Multiplication(*) :", a*b)
+print("Division(/) :", a/b)
+print("Floor Division(//) :", a//b)
+print("Modulus(%) :", a%b)
+print("Exponentiation(**) :", a**b)
